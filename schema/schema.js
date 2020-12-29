5629503e939a0fc5a09bbd380717cae1e3b753e7
Makdoom Shaikh <makshaikh99@gmail.com>
2020-12-29 16:01:06 +0530

    Added first basic event

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,19 @@
+const { buildSchema } = require("graphql");
 
+const schema = buildSchema(`
+  type RootQuery {
+    events: [String!]!
+  }
+
+  type RootMutations {
+    createEvent(name: String!): String
+
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutations
+  }
+`);
+
+module.exports = schema;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-import mongoose from "mongoose";
-import schema from "./schema/schema.js";
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+// const schema = require("./schema/schema");
 
 const app = express();
 
@@ -9,7 +9,31 @@ const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema,
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutations {
+        createEvent(name: String!): String
+
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutations
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Makdoom", "Mahek", "Ahad"];
+      },
+
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
     graphiql: true,
   })
 );

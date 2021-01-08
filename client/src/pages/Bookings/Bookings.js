138e278a3a86a56ddc60a290c52a97dac432be45
Makdoom Shaikh <makshaikh99@gmail.com>
2021-01-08 19:36:58 +0530

    Demo graphql server is all set

--- client/src/pages/Bookings/Bookings.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Bookings = () => {
-  return <div>Booking component</div>;
-};
-
-export default Bookings;
--- client/src/pages/Events/Events.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Events = () => {
-  return <div>Event component</div>;
-};
-
-export default Events;
